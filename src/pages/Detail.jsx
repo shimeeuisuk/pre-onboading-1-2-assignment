9afe057edc556ac89ae310f38239a34d5617362d
shimeeuisuk <euisuk95@gmail.com>
2022-10-30 10:39:36 +0900

    [feat] 페이지에서 컴포넌트 조합

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import IssueContent from '../components/IssueContent/IssueContent';
 
 const Detail = () => {
-  return <div>Detail</div>;
+  return (
+    <div>
+      <IssueContent />
+    </div>
+  );
 };
 
 export default Detail;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import IssueList from '../components/IssueList/IssueList';
+
+const Home = () => {
+  return <IssueList />;
+};
+
+export default Home;
